a42866175c3a570b9d342b62ccf3b5dc9c4a6b63
Yijun Lin <yijunlin@Yijuns-MacBook-Pro.local>
2021-01-30 18:41:12 -0800

    week 1

---
+++ main.py
@@ -0,0 +1,16 @@
+import pyspark
+
+
+if __name__ == '__main__':
+
+    sc_conf = pyspark.SparkConf() \
+        .setAppName('task1') \
+        .setMaster('local[*]') \
+        .set('spark.driver.memory', '8g') \
+        .set('spark.executor.memory', '4g')
+
+    sc = pyspark.SparkContext(conf=sc_conf)
+    sc.setLogLevel("OFF")
+
+
+
